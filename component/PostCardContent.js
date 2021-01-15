b4317c9a784fe575251ffd416bd448e91ebb0a23
sjs <seo777111@gmail.com>
2021-01-15 19:16:41 +0900

    post hash link

--- component/PostCardContent.js
+++ component/PostCardContent.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 
+// 해시태그 링크 걸기
 const PostCardContent = ({postData}) => {
+    console.log(postData)
     return (
         <div>
-            
+            { postData.split(/(#[^\s#]+)/g).map((v,i)=>{
+                return(
+                    <Link href={`/hash/${v.slice(1)}`} key={i}><a>{v}</a></Link> //v.slice(1)= #을 제외한 단어
+                )
+            })
+
+            }
         </div>
     );
 };
